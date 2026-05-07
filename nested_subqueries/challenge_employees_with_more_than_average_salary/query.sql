2f2fbeb6a364ce70faf939db73deb371977f229d
vikram-21 <>
2026-05-07 09:05:00 +0000

    Challenge: Employees With More Than Average Salary

---
+++ nested_subqueries/challenge_employees_with_more_than_average_salary/query.sql
@@ -0,0 +1,9 @@
+SELECT first_name,
+        last_name,
+        salary
+FROM employees
+WHERE salary > (
+                SELECT AVG(salary)
+                FROM employees
+)
+ORDER BY salary DESC;
